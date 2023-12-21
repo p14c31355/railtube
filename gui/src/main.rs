c871a16bdcc53926723d32f4a771adff25f8d329
Placeless <mikoprpr@hotmail.com>
2023-12-22 02:19:24 +0900

    コピペキッズ

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -17,6 +17,39 @@ fn main() {
                 transform: Transform::from_xyz(0.0, 6., 12.0)
                     .looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
                 ..default()
+            // 光を追加
+commands.spawn(PointLightBundle {
+    point_light: PointLight {
+        intensity: 9000.0,
+        range: 100.,
+        shadows_enabled: true,
+        ..default()
+    },
+    transform: Transform::from_xyz(8.0, 16.0, 8.0),
+    ..default()
+});
+// 光を追加
+commands.spawn(PointLightBundle {
+    point_light: PointLight {
+        intensity: 9000.0,
+        range: 100.,
+        shadows_enabled: true,
+        ..default()
+    },
+    transform: Transform::from_xyz(8.0, 16.0, 8.0),
+    ..default()
+});
+fn setup(..., mut meshes: ResMut<Assets<Mesh>>) {
+    ...
+    let sphere = meshes.add(shape::UVSphere::default().into());
+
+    commands.spawn(PbrBundle {
+        mesh: sphere,
+        // このxyzはカメラの向きと同じ
+        transform: Transform::from_xyz(0.0, 1.0, 0.0),
+        ..default()
+    });
+}
             });
         }
     }
