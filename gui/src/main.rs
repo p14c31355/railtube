aa9654883aba104b62d5072d768630469cfce72a
Placeless <mikoprpr@hotmail.com>
2023-12-22 02:43:41 +0900

    ネストをガチャガチャするなどした

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
         App::new().run();
     }
 
-    fn main() {
+    fn Camera3dBundle() {
         App::new()
             .add_plugins(DefaultPlugins)
             .add_systems(Startup, setup)
@@ -16,22 +16,23 @@ fn main() {
             commands.spawn(Camera3dBundle {
                 transform: Transform::from_xyz(0.0, 6., 12.0)
                     .looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
-                ..default())
-            };
+            });
             // 光を追加
-commands.spawn(PointLightBundle {
-    point_light: PointLight {
-        intensity: 9000.0,
-        range: 100.,
-        shadows_enabled: true,
-        ..default())
-    }},
-    transform: Transform::from_xyz(8.0, 16.0, 8.0),
-    ..default()
-    let sphere = meshes.add(shape::UVSphere::default().into());
-    commands.spawn(PbrBundle {
-        mesh: sphere,
-        // このxyzはカメラの向きと同じ
-        transform: Transform::from_xyz(0.0, 1.0, 0.0),
-        ..default()
-});
+            commands.spawn(PointLightBundle {
+                point_light: PointLight {
+                    intensity: 9000.0,
+                    range: 100.,
+                    shadows_enabled: true,
+                    transform: Transform::from_xyz(8.0, 16.0, 8.0),
+                },
+            });
+            let sphere = meshes.add(shape::UVSphere::default().into());
+            commands.spawn(PbrBundle {
+                mesh: sphere,
+                // このxyzはカメラの向きと同じ
+                transform: Transform::from_xyz(0.0, 1.0, 0.0),
+                ..default()
+            })
+        }
+    }
+}
