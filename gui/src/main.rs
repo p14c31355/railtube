fb1da62bea440017f5ddbd15e4153830f746e542
Placeless <mikoprpr@hotmail.com>
2023-12-21 21:27:50 +0900

    ボタン追加

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,5 +1,23 @@
-use::bevy::prelude::*;
+use ::bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    fn nothing() {
+        App::new().run();
+    }
+
+    fn main() {
+        App::new()
+            .add_plugins(DefaultPlugins)
+            .add_systems(Startup, setup)
+            .run();
+
+        fn setup(mut commands: Commands) {
+            // カメラを追加
+            commands.spawn(Camera3dBundle {
+                transform: Transform::from_xyz(0.0, 6., 12.0)
+                    .looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
+                ..default()
+            });
+        }
+    }
 }
