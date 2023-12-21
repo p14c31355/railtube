0b38f2f400070ea9f3147430be2487ccebbb2728
Placeless <mikoprpr@hotmail.com>
2023-12-22 02:27:22 +0900

    リファクタリングまがいの事を……

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -11,46 +11,27 @@ fn main() {
             .add_systems(Startup, setup)
             .run();
 
-        fn setup(mut commands: Commands) {
+        fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
             // カメラを追加
             commands.spawn(Camera3dBundle {
                 transform: Transform::from_xyz(0.0, 6., 12.0)
                     .looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
-                ..default()
+                ..default())
+            };
             // 光を追加
 commands.spawn(PointLightBundle {
     point_light: PointLight {
         intensity: 9000.0,
         range: 100.,
         shadows_enabled: true,
-        ..default()
-    },
+        ..default())
+    }},
     transform: Transform::from_xyz(8.0, 16.0, 8.0),
     ..default()
-});
-// 光を追加
-commands.spawn(PointLightBundle {
-    point_light: PointLight {
-        intensity: 9000.0,
-        range: 100.,
-        shadows_enabled: true,
-        ..default()
-    },
-    transform: Transform::from_xyz(8.0, 16.0, 8.0),
-    ..default()
-});
-fn setup(..., mut meshes: ResMut<Assets<Mesh>>) {
-    ...
     let sphere = meshes.add(shape::UVSphere::default().into());
-
     commands.spawn(PbrBundle {
         mesh: sphere,
         // このxyzはカメラの向きと同じ
         transform: Transform::from_xyz(0.0, 1.0, 0.0),
         ..default()
-    });
-}
-            });
-        }
-    }
-}
+});
