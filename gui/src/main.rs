640362f9d61d59dcfa23f594d9eec492525a3e5b
Placeless <mikoprpr@hotmail.com>
2023-12-23 10:16:17 +0900

    もはや何も分からん……

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,3 +1,19 @@
+/*
+Copyright 2023 YoshitakaNaraoka
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 use bevy::app::App;
 use bevy::app::Startup;
 use bevy::asset::Assets;
@@ -15,7 +31,7 @@ use bevy::DefaultPlugins;
 use bevy::render::mesh::shape::UVSphere;
 use crate::UVSphere;
 
-bevy::prelude::PointLight::*();
+bevy::prelude::PointLight!();
 
 fn main() {
     fn nothing() {
