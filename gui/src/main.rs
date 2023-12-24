e2f922162f3220852e6d937bedc60e5fd31bd6b3
Placeless <mikoprpr@hotmail.com>
2023-12-25 04:07:13 +0900

    合ってるのか？

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -14,6 +14,21 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+use bevy::app::App;
+use bevy::DefaultPlugins;
+use bevy::app::Startup;
+use bevy::ecs::system::Commands;
+use bevy::ecs::system::ResMut;
+use bevy::asset::Assets;
+use bevy::render::mesh::Mesh;
+use bevy::core_pipeline::core_3d::Camera3dBundle;
+use bevy::transform::components::Transform;
+use bevy::math::Vec3;
+use bevy::pbr::PointLightBundle;
+use bevy::pbr::PointLight;
+use bevy::render::mesh::shape::UVSphere;
+use bevy::pbr::PbrBundle;
+use bevy::utils::default;
 
 #[warn(unused_imports)]
 fn main() {
